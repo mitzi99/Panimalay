10d0058a0e651f0b4e92d72176226821e5dbc0c6
Mitzi Dorato <dorato92@gmaol.com>
2020-12-03 16:25:28 +0800

    sign in/up added

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,12 @@ from app import app
 @app.route('/')
 def home():
 	return render_template('index.html')
-	#return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+	return render_template('signup.html')
+
+@app.route('/signin')
+def signin():
+	return render_template('signin.html')
